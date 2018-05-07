b22ac131168c62ac758fd1f43ea6a8c2d7ba830d
duncanhunter <duncan@duncanhunter.com.au>
2018-05-06 21:42:44 -0500

    part-5

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { LoginComponent } from './containers/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth/auth.service';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 
 export const authRoutes: Route[] = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent }
 ];
-
 const COMPONENTS = [LoginComponent, LoginFormComponent];
+
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   declarations: [COMPONENTS],
-  exports: [COMPONENTS]
+  exports: [COMPONENTS],
+  providers: [AuthService]
 })
 export class AuthModule {}
---
+++ libs/auth/src/services/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Authenticate } from '@demo-app/data-models';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  login(authenticate: Authenticate): Observable<any> {
+    return this.httpClient.post('http:localhost:3000/login', authenticate);
+  }
+}

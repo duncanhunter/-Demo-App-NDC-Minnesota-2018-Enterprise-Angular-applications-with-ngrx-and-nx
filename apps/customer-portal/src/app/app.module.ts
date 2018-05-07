acf822a83bb8c9e04f0c201ad82f18ed285bc26d
duncanhunter <duncan@duncanhunter.com.au>
2018-05-06 21:49:30 -0500

    add material packages

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { authRoutes, AuthModule } from '@demo-app/auth';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NxModule.forRoot(),
     RouterModule.forRoot([{path: 'auth', children: authRoutes}], { initialNavigation: 'enabled' }),
     AuthModule

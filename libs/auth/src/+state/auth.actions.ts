3b462d10a4d4e6d2151f434769a3a32fce46b38d
duncanhunter <duncan@duncanhunter.com.au>
2018-05-07 07:28:04 -0500

    change action names

--- libs/auth/src/+state/auth.actions.ts
+++ libs/auth/src/+state/auth.actions.ts
@@ -2,34 +2,34 @@ import { Action } from '@ngrx/store';
 import { User, Authenticate } from '@demo-app/data-models';
 
 export enum AuthStateActionTypes {
-Login = '[AuthState] Login',
-LoginSuccess = '[AuthState] Login Success',
-LoginFail = '[AuthState] Login Fail',
-NavigateToProfile = '[AuthState] Navigate To Profile'
+  Login = '[Login Page] Login',
+  LoginSuccess = '[Auth API] Login Success',
+  LoginFail = '[Auth API] Login Fail',
+  NavigateToProfile = '[Login Page] Navigate To Profile'
 }
 
 export class LoginAction implements Action {
-readonly type = AuthStateActionTypes.Login;
-constructor(public payload: Authenticate) {}
+  readonly type = AuthStateActionTypes.Login;
+  constructor(public payload: Authenticate) {}
 }
 
 export class LoginSuccessAction implements Action {
-readonly type = AuthStateActionTypes.LoginSuccess;
-constructor(public payload: User) {}
+  readonly type = AuthStateActionTypes.LoginSuccess;
+  constructor(public payload: User) {}
 }
 
 export class LoginFailAction implements Action {
-readonly type = AuthStateActionTypes.LoginFail;
-constructor(public payload) {}
+  readonly type = AuthStateActionTypes.LoginFail;
+  constructor(public payload) {}
 }
 
 export class NavigateToProfileAction implements Action {
-readonly type = AuthStateActionTypes.NavigateToProfile;
-constructor(public payload:number) {}
+  readonly type = AuthStateActionTypes.NavigateToProfile;
+  constructor(public payload: number) {}
 }
 
 export type AuthStateActions =
-LoginAction
-| LoginFailAction
-| LoginSuccessAction
-| NavigateToProfileAction;
+  | LoginAction
+  | LoginFailAction
+  | LoginSuccessAction
+  | NavigateToProfileAction;

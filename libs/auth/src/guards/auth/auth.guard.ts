bbfffe5dc22ff8aeffe33740d4698fda7be66198
duncanhunter <duncan@duncanhunter.com.au>
2018-05-06 23:12:47 -0500

    remove debugger

--- libs/auth/src/guards/auth/auth.guard.ts
+++ libs/auth/src/guards/auth/auth.guard.ts
@@ -14,7 +14,6 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if(this.authService.isAuthenticated) {
-      debugger;
       return true;
     } else {
       this.router.navigate([`/auth/login`]);

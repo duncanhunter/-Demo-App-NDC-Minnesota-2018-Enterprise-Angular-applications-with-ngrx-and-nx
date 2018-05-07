accac4d12493332de8e8979c3735c5eb95ae5c5f
duncanhunter <duncan@duncanhunter.com.au>
2018-05-06 22:11:07 -0500

    fix http url

--- libs/auth/src/services/auth/auth.service.ts
+++ libs/auth/src/services/auth/auth.service.ts
@@ -8,6 +8,6 @@ export class AuthService {
   constructor(private httpClient: HttpClient) {}
 
   login(authenticate: Authenticate): Observable<any> {
-    return this.httpClient.post('http:localhost:3000/login', authenticate);
+    return this.httpClient.post('http://localhost:3000/login', authenticate);
   }
 }

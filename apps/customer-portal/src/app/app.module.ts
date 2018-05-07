b81607e9bbee7d2084db9db45bb94cd5bc6f6576
duncanhunter <duncan@duncanhunter.com.au>
2018-05-06 19:57:01 -0500

    add initial auth lib

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { authRoutes } from '@demo-app/auth';
 
 @NgModule({
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([{path: 'auth', children: authRoutes}], { initialNavigation: 'enabled' })
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
